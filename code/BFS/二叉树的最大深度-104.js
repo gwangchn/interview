b81b5ad4791da221b88530e0c7bf9449d4fd8222
guanwang <928456052@qq.com>
2021-08-23 18:12:43 +0800

    ✨feat:BFS

---
+++ code/BFS/二叉树的最大深度-104.js
@@ -0,0 +1,35 @@
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0
+  let deep = 0
+  let queue = [root]
+  
+  while (queue.length) {
+    ++deep
+    let len = queue.length
+    while (len--) {
+      let node = queue.shift()
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+  }
+
+  return deep
+};
