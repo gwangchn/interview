a2b0d5fd26224bfcc59d940f84971f74d50afcf7
guanwang <928456052@qq.com>
2021-08-24 16:24:09 +0800

    ✨feat:贪心算法

---
+++ code/贪心算法/买卖股票的最佳时机 II-122.js
@@ -0,0 +1,14 @@
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let ans = 0;
+  let n = prices.length;
+  for (let i = 1; i < n; ++i) {
+      ans += Math.max(0, prices[i] - prices[i - 1]);
+  }
+  return ans;
+};
