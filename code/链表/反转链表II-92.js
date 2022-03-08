e9f5b7700f3862cfe9d7b66c2709827d7dc416d4
guanwang <928456052@qq.com>
2022-03-08 16:03:48 +0800

    ✨feat:反转链表二

---
+++ code/链表/反转链表II-92.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+let reverseBetween = function(head, left, right) {
+  // 设置 dummyNode 是这一类问题的一般做法
+  const dummy_node = new ListNode(-1);
+  dummy_node.next = head;
+  let pre = dummy_node;
+  for (let i = 0; i < left - 1; ++i) {
+    pre = pre.next;
+  }
+
+  let cur = pre.next;
+  for (let i = 0; i < right - left; ++i) {
+    const next = cur.next;
+    cur.next = next.next;
+    next.next = pre.next;
+    pre.next = next;
+  }
+  return dummy_node.next;
+};
