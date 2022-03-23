8f2ac0979473dd8093be252e1d71febbea31e13e
guanwang <928456052@qq.com>
2022-03-23 10:54:06 +0800

    🖖feat:删除二叉搜索树中的节点

---
+++ code/二叉树/删除二叉搜索树中的节点-450.js
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode-cn.com/problems/delete-node-in-a-bst/
+ * 
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+let deleteNode = function(root, key) {
+  if (root === null)
+    return root;
+  if (root.val === key) {
+    if (!root.left)
+      return root.right;
+    else if (!root.right)
+      return root.left;
+    else {
+      let cur = root.right;
+      while (cur.left) {
+        cur = cur.left;
+      }
+      cur.left = root.left;
+      root = root.right;
+      delete root;
+      return root;
+    }
+  }
+  if (root.val > key)
+    root.left = deleteNode(root.left, key);
+  if (root.val < key)
+    root.right = deleteNode(root.right, key);
+  return root;
+};
