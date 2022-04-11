0c32a6fdda889ddb998d2ab12869a679db458246
guanwang <928456052@qq.com>
2022-04-11 10:25:31 +0800

    🖖feat: 无重叠区间

---
+++ code/贪心算法/无重叠区间-435.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+let eraseOverlapIntervals = function(intervals) {
+  intervals.sort((a, b) => a[1] - b[1])
+
+  const n = intervals.length
+  let right = intervals[0][1]
+  let ans = 1
+
+  for (let i = 1; i < n; ++i) {
+    if (intervals[i][0] >= right) {
+      ++ans
+      right = intervals[i][1]
+    }
+  }
+
+  return n - ans
+}
