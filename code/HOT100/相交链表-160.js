289f0be1953697a18f4cba6813c4c904efc83d54
guanwang <928456052@qq.com>
2022-06-17 10:12:34 +0800

    ✨feat: 相交链表

---
+++ code/HOT100/相交链表-160.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+let getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) return null
+  let pA = headA
+  let pB = headB
+  while (pA !== pB) {
+    pA = pA === null ? headB : pA.next
+    pB = pB === null ? headA : pB.next
+  }
+  return pA
+}
