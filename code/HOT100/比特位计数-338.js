eef47d03394e2950cbebee4f7a31de798fa4ae8b
guanwang <928456052@qq.com>
2022-07-11 15:06:44 +0800

    feat: ✨比特位计数

---
+++ code/HOT100/比特位计数-338.js
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.cn/problems/counting-bits/
+ *
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  const bits = new Array(n + 1).fill(0)
+  for (let i = 1; i <= n; i++) {
+    bits[i] = bits[i & (i - 1)] + 1
+  }
+  return bits
+}
