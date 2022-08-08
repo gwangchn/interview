01a17a7a91150a8c1ffd8b3bf9887ba8f482fdc8
guanwang <928456052@qq.com>
2022-08-08 14:10:18 +0800

    feat: ✨格雷编码

---
+++ code/tencent50/格雷编码-89.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.cn/problems/gray-code/
+ *
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function (n) {
+  let res = []
+  for (let i = 0; i < 1 << n; i++) res.push(i ^ (i >> 1))
+  return res
+}
