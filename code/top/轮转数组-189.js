b92c3c91f2d0e202a952cde9ff7d24baeaa08873
guanwang <928456052@qq.com>
2022-09-20 11:33:46 +0800

    feat: 🚀 轮转数组

---
+++ code/top/轮转数组-189.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const gcd = (x, y) => (y ? gcd(y, x % y) : x)
+
+var rotate = function (nums, k) {
+  const n = nums.length
+  k = k % n
+  let count = gcd(k, n)
+  for (let start = 0; start < count; ++start) {
+    let current = start
+    let prev = nums[start]
+    do {
+      const next = (current + k) % n
+      const temp = nums[next]
+      nums[next] = prev
+      prev = temp
+      current = next
+    } while (start !== current)
+  }
+}
