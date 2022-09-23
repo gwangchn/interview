a532b1fe70b76745ba040c0492c08ba47dbc1b9b
guanwang <928456052@qq.com>
2022-09-23 10:43:59 +0800

    feat: 🚀 位1的个数

---
+++ code/top/位1的个数-191.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.cn/problems/number-of-1-bits/
+ *
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let ret = 0
+  while (n) {
+    n &= n - 1
+    ret++
+  }
+  return ret
+}
