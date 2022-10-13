84aedba1a1867421ea579ed8b3921804b056f109
guanwang <928456052@qq.com>
2022-10-13 10:46:00 +0800

    feat: 🚀 奇偶链表

---
+++ code/top/奇偶链表-328.js
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.cn/problems/odd-even-linked-list/description/?favorite=2ckc81c
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (!head) return head
+  let evenHead = head.next
+  let odd = head
+  let even = evenHead
+
+  while (even !== null && even.next !== null) {
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+
+  return head
+}
