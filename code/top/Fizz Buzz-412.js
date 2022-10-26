b9501a44fec956241a440dd61c3c650d40ac67d2
guanwang <928456052@qq.com>
2022-10-26 10:44:20 +0800

    feat: 🚀 Fizz Buzz

---
+++ code/top/Fizz Buzz-412.js
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.cn/problems/fizz-buzz/description/
+ *
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  const answer = []
+  for (let i = 1; i <= n; i++) {
+    const sb = []
+    if (i % 3 === 0) {
+      sb.push('Fizz')
+    }
+    if (i % 5 === 0) {
+      sb.push('Buzz')
+    }
+    if (sb.length === 0) {
+      sb.push(i)
+    }
+    answer.push(sb.join(''))
+  }
+  return answer
+}
